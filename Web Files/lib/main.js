0944d32f0e5a421456264500fbbb069ad750636a
Verzweifler <fabian.freundorfer@farchet.de>
2015-01-02 16:55:50 +0100

    replaced jquery 1.11.1 with jquery 2.1.3
    added base URL for www.medien.ifi.lmu.de, since given URLs only contain e.g. "/forschung/..."

--- Web Files/lib/main.js
+++ Web Files/lib/main.js
@@ -1,11 +1,42 @@
 $(document).ready(function() {
 
+	// Base-URL on which the URLs in the Publication-Objects build
+	var baseURL = "http://www.medien.ifi.lmu.de/";
+
 	var publicationsJSON = [],
 		authorsJSON = [];
 
+	// Request "mortifier" asks for the data
 	$.get("http://localhost:3000/mortifier", function(data) {
 		if(data.bool){
+			// Publication format:
+			/*
+			{"id":"pub_1",
+			"year":"2015",
+			"authors":[
+				{"name":"Daniel Buschek",
+					"url":"http://www.medien.ifi.lmu.de/team/daniel.buschek/"},
+				{"name":"Alexander De Luca",
+					"url":"http://www.medien.ifi.lmu.de/team/alexander.de.luca/"},
+				{"name":"Florian Alt",
+					"url":"http://www.medien.ifi.lmu.de/team/florian.alt/"}],
+			"title":{
+				"url":"/forschung/publikationen/detail?pub=buschek2015chi",
+				"name":"Improving Accuracy, Applicability and Usability of Keystroke Biometrics on Mobile Touchscreen Devices"},
+			"description":{"html":""},
+			"bibfile":"/pubdb/publications/pub/buschek2015chi/buschek2015chi.bib",
+			"downloads":[],
+			"award":false,
+			"keywords":["keystroke dynamics","mobile","touch","biometrics"]}
+			*/
 			publicationsJSON = data.publications;
+
+			// Authors format:
+			/*
+			{"name":"Daniel Buschek",
+			"publications":["pub_1","pub_2","pub_11","pub_53","pub_91","pub_92"],
+			"url":"http://www.medien.ifi.lmu.de/team/daniel.buschek/"}
+			*/
 			authorsJSON = data.authors;
 			$('img').hide();
 			$('#publications').val(JSON.stringify(data.publications)).show();
@@ -16,6 +47,4 @@ $(document).ready(function() {
 		}
 	})
 
-
 });
-	
